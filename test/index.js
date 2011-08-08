e9e1c223096ad2bc6b02acacc15b7bc55bdba2cf
Mariusz Nowak <medyk@medikoo.com>
2011-08-08 14:02:08 +0200

    Tests TAD way

--- test/index.js
+++ test/index.js
@@ -2,20 +2,11 @@
 
 var msg = 'test test';
 
-module.exports = {
-	"Plain": function (t, a) {
-		a.equal(t("test"), "test");
-	},
-	"Foreground": function (t, a) {
-		a.equal(t.red(msg), '\x1b[31m' + msg + '\x1b[39m');
-	},
-	"Background": function (t, a) {
-		a.equal(t.bgRed(msg), '\x1b[41m' + msg + '\x1b[49m');
-	},
-	"Format": function (t, a) {
-		a.equal(t.bold(msg), '\x1b[1m' + msg + '\x1b[22m');
-	},
-	"Foreground & Format": function (t, a) {
-		a.equal(t.bold.blue(msg), '\x1b[34m\x1b[1m' + msg + '\x1b[22m\x1b[39m');
-	}
+module.exports = function (t, a) {
+	a(t("test"), "test", "Plain");
+	a(t.red(msg), '\x1b[31m' + msg + '\x1b[39m', "Foreground");
+	a(t.bgRed(msg), '\x1b[41m' + msg + '\x1b[49m', "Background");
+	a(t.bold(msg), '\x1b[1m' + msg + '\x1b[22m', "Format");
+	a(t.bold.blue(msg), '\x1b[34m\x1b[1m' + msg + '\x1b[22m\x1b[39m',
+		"Foreground & Format");
 };
