3a4eaf9de00b6c5c2db96910635640ab6f352b88
Mariusz Nowak <mariusz@medikoo.com>
2012-01-22 19:00:02 +0100

    More reliable throbber tests

--- test/throbber.js
+++ test/throbber.js
@@ -2,84 +2,37 @@
 
 var spawn = require('child_process').spawn
   , pg = __dirname + '/__playground'
-  , initChars = '-\\|/';
 
 module.exports = {
 	"": function (a, d) {
 		var t = spawn(pg + '/throbber.js')
-		  , out = ""
-		  , err = ""
-		  , chars = initChars
-		  , i, wrote = false
-		  , watch = function () {
-				setTimeout(function () {
-					i = setInterval(function () {
-						var expected = "";
-						if (wrote) {
-							expected += '\u0008';
-						}
-						wrote = true;
-						expected += chars[0];
-						chars = chars.slice(1);
-						a(out, expected);
-						out = "";
-					}, 100);
-				}, 50);
-			},
-		initialized = false;
+		  , out = [], err = '', time = Date.now();
 
 		t.stdout.on('data', function (data) {
-			if (!initialized) {
-				initialized = true;
-				watch();
-			} else {
-				out += data;
-			}
+			out.push(data);
 		});
 		t.stderr.on('data', function (data) {
 			err += data;
 		});
 		t.on('exit', function () {
-			clearInterval(i);
+			a.ok(out.length > 4, "Interval");
+			a(out.join(""), "START-\b\\\b|\b/\b-\b", "Output")
 			a(err, "", "No stderr output"); d();
 		});
 	},
 	"Formatted": function (a, d) {
 		var t = spawn(pg + '/throbber.formatted.js')
-		  , out = ""
-		  , err = ""
-		  , chars = initChars
-		  , i, wrote = false
-		  , watch = function () {
-				setTimeout(function () {
-					i = setInterval(function () {
-						var expected = "";
-						if (wrote) {
-							expected += '\u0008';
-						}
-						wrote = true;
-						expected += '\x1b[31m' + chars[0] + '\x1b[39m';
-						chars = chars.slice(1);
-						a(out, expected);
-						out = "";
-					}, 100);
-				}, 50);
-			},
-		initialized = false;
+		  , out = [], err = '', time = Date.now();
 
 		t.stdout.on('data', function (data) {
-			if (!initialized) {
-				initialized = true;
-				watch();
-			} else {
-				out += data;
-			}
+			out.push(data);
 		});
 		t.stderr.on('data', function (data) {
 			err += data;
 		});
 		t.on('exit', function () {
-			clearInterval(i);
+			a.ok(out.length > 4, "Interval");
+			a(out.join(""), "START\x1b[31m-\x1b[39m\b\x1b[31m\\\x1b[39m\b\x1b[31m|\x1b[39m\b\x1b[31m/\x1b[39m\b\x1b[31m-\x1b[39m\b", "Output")
 			a(err, "", "No stderr output"); d();
 		});
 	}
