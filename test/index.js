7d3c8504ea223fe563c5ac91ba092f9f58f33018
Mariusz Nowak <mariusz@medikoo.com>
2012-08-16 17:44:57 +0200

    Tests for bright variants

--- test/index.js
+++ test/index.js
@@ -9,6 +9,8 @@ module.exports = function (t, a) {
 	a(t.bold(msg), '\x1b[1m' + msg + '\x1b[22m', "Format");
 	a(t.bold.blue(msg), '\x1b[34m\x1b[1m' + msg + '\x1b[22m\x1b[39m',
 		"Foreground & Format");
+	a(t.red.bright(msg), '\x1b[91m' + msg + '\x1b[39m', "Bright");
+	a(t.bgRed.bgBright(msg), '\x1b[101m' + msg + '\x1b[49m', "Bright background");
 	a(typeof t.width, 'number', "Width");
 	a(typeof t.height, 'number', "Height");
 };
