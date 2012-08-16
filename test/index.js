132527b764ff3edb653c0e82ad72fec212535569
Mariusz Nowak <mariusz@medikoo.com>
2012-08-16 19:00:57 +0200

    Reorganized move algorithm.
    Do not output control string for zero movement

--- test/index.js
+++ test/index.js
@@ -17,24 +17,24 @@ module.exports = function (t, a) {
 
 	a(/\n*\x1bc/.test(t.reset), true, "Reset");
 
-	a(t.up(), '\x1b[0A', "Up: No argument");
-	a(t.up({}), '\x1b[0A', "Up: Not a number");
-	a(t.up(-34), '\x1b[0A', "Up: Negative");
+	a(t.up(), '', "Up: No argument");
+	a(t.up({}), '', "Up: Not a number");
+	a(t.up(-34), '', "Up: Negative");
 	a(t.up(34), '\x1b[34A', "Up: Positive");
 
-	a(t.down(), '\x1b[0B', "Down: No argument");
-	a(t.down({}), '\x1b[0B', "Down: Not a number");
-	a(t.down(-34), '\x1b[0B', "Down: Negative");
+	a(t.down(), '', "Down: No argument");
+	a(t.down({}), '', "Down: Not a number");
+	a(t.down(-34), '', "Down: Negative");
 	a(t.down(34), '\x1b[34B', "Down: Positive");
 
-	a(t.right(), '\x1b[0C', "Right: No argument");
-	a(t.right({}), '\x1b[0C', "Right: Not a number");
-	a(t.right(-34), '\x1b[0C', "Right: Negative");
+	a(t.right(), '', "Right: No argument");
+	a(t.right({}), '', "Right: Not a number");
+	a(t.right(-34), '', "Right: Negative");
 	a(t.right(34), '\x1b[34C', "Right: Positive");
 
-	a(t.left(), '\x1b[0D', "Left: No argument");
-	a(t.left({}), '\x1b[0D', "Left: Not a number");
-	a(t.left(-34), '\x1b[0D', "Left: Negative");
+	a(t.left(), '', "Left: No argument");
+	a(t.left({}), '', "Left: Not a number");
+	a(t.left(-34), '', "Left: Negative");
 	a(t.left(34), '\x1b[34D', "Left: Positive");
 
 	a(t.move(), '', "Move: No arguments");
