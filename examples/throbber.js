704978f214a9cc8265dba92daf6aebf742945b59
Lars-Magnus Skog <ralphtheninja@riseup.net>
2015-06-06 19:36:11 +0200

    add examples from readme

---
+++ examples/throbber.js
@@ -0,0 +1,13 @@
+var setupThrobber = require('../throbber');
+
+var throbber = setupThrobber(function (str) {
+  process.stdout.write(str);
+}, 200);
+
+process.stdout.write('Throbbing for 3 seconds here -> ');
+throbber.start();
+
+setTimeout(function () {
+  console.log();
+  throbber.stop();
+}, 3000);
---
+++ examples/xterm.js
@@ -0,0 +1,4 @@
+var clc = require('../');
+
+var msg = clc.xterm(202).bgXterm(236);
+console.log(msg('Orange text on dark gray background'));
