65aac0012b796be476e918e8201f0c4e5e96b9aa
StreetStrider <teamfortresslife@gmail.com>
2015-09-06 02:08:56 +0300

    added tests for simple nested styles.

--- test/slice.js
+++ test/slice.js
@@ -30,4 +30,9 @@ module.exports = function (t, a) {
 	a(slice('ABC\x1b[41mDEF\x1b[49m', 0, 5), "ABC\x1b[41mDE\x1b[49m", "Save styles when chopping part of the backcolor-styled string");
 	a(slice('ABC\x1b[41mDEF\x1b[49m', 1, 4),  "BC\x1b[41mD\x1b[49m", "Save styles when chopping part of the backcolor-styled string");
 	a(slice('ABC\x1b[41mDEF\x1b[49m', 1, 6),  "BC\x1b[41mDEF\x1b[49m", "Save styles when chopping part of the backcolor-styled string");
+
+	a(slice('\x1b[1mAAA\x1b[31mBBB\x1b[39mAAA\x1b[22m', 0, 5), "\x1b[1mAAA\x1b[31mBB\x1b[39m\x1b[22m", "Works with nested styles")
+	a(slice('\x1b[1mAAA\x1b[31mBBB\x1b[39mAAA\x1b[22m', 2, 7), "\x1b[1mA\x1b[31mBBB\x1b[39mA\x1b[22m", "Works with nested styles")
+	a(slice('\x1b[3mAAA\x1b[41mBBB\x1b[49mAAA\x1b[23m', 0, 5), "\x1b[3mAAA\x1b[41mBB\x1b[49m\x1b[23m", "Works with nested styles")
+	a(slice('\x1b[3mAAA\x1b[41mBBB\x1b[49mAAA\x1b[23m', 2, 7), "\x1b[3mA\x1b[41mBBB\x1b[49mA\x1b[23m", "Works with nested styles")
 };
