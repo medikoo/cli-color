1d603f380dee9fd2b47927827d065d1a3a7ce4a3
StreetStrider <teamfortresslife@gmail.com>
2015-09-16 00:37:24 +0300

    added tests for CSI reducing cases.

--- test/slice.js
+++ test/slice.js
@@ -28,12 +28,6 @@ module.exports = function (t, a) {
 	a(slice('\x1b[41mABCDE\x1b[49m', -3, -1), "\x1b[41mCD\x1b[49m",
 		"Works on whole single backcolor-styled string");
 
-	a(slice('ABC\x1b[31mDEF\x1b[39m', 3), "\x1b[31mDEF\x1b[39m", "Does not corrupt start CSI");
-	//a(slice('ABC\x1b[31mDEF\x1b[39m', 0, 3), "ABC", "Does not mixin CSI to plain string");
-
-	a(slice('\x1b[41mABC\x1b[49mDEF', 0, 3), "\x1b[41mABC\x1b[49m", "Does not corrupt end CSI");
-	//a(slice('\x1b[41mABC\x1b[49mDEF', 3), "DEF", "Does not mixin CSI to plain string");
-
 	a(slice('ABC\x1b[31mDEF\x1b[39m', 0, 5), "ABC\x1b[31mDE\x1b[39m",
 		"Save styles when chopping part of the forecolor-styled string");
 	a(slice('ABC\x1b[31mDEF\x1b[39m', 1, 4), "BC\x1b[31mD\x1b[39m",
@@ -73,4 +67,26 @@ module.exports = function (t, a) {
 	a(slice('\x1b[31mAAA\x1b[32mBBB\x1b[31mAAA\x1b[39m', -8, -1),
 		"\x1b[31mAA\x1b[32mBBB\x1b[31mAA\x1b[39m",
 		"Works with nested overlapping styles");
+
+	/* CSI at the edge of slice */
+	a(slice('ABC\x1b[31mDEF\x1b[39m', 3), "\x1b[31mDEF\x1b[39m", "Does not corrupt start CSI");
+	a(slice('\x1b[41mABC\x1b[49mDEF', 0, 3), "\x1b[41mABC\x1b[49m", "Does not corrupt end CSI");
+
+	/* CSI-reducing tests */
+	a(slice('ABC\x1b[31mDEF\x1b[39m', 0, 3), "ABC", "Does not mixin CSI to plain string");
+	a(slice('ABCD\x1b[31mEF\x1b[39m', 0, 3), "ABC", "Does not mixin CSI to plain string");
+	a(slice('\x1b[41mABC\x1b[49mDEF', 3), "DEF", "Does not mixin CSI to plain string");
+	a(slice('\x1b[41mAB\x1b[49mCDEF', 3), "DEF", "Does not mixin CSI to plain string");
+
+	a(slice('A\x1b[31mBBB\x1b[39mD\x1b[32mE\x1b[39m', 2, 3), "\x1b[31mB\x1b[39m",
+		"Does slice with surrounding styles");
+	a(slice('A\x1b[31mBBB\x1b[39mD\x1b[32mE\x1b[39m', 0, 5), "A\x1b[31mBBB\x1b[39mD",
+		"Does slice with inner styles");
+
+	a(slice('A\x1b[31mBBB\x1b[39m\x1b[2J\x1b[0;0H', 0, 4), "A\x1b[31mBBB\x1b[39m",
+		"Remove reset CSI");
+
+	// 'A' + clc.red('BBB') + clc.erase.screen + clc.move.to(0, 0)
+	a(slice('A\u001b[31mBBB\u001b[39m\u001b[2J\u001b[1;1H', 0, 4), "A\x1b[31mBBB\x1b[39m",
+		"Remove control CSIs");
 };
