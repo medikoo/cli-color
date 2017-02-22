d2f0c48eae053cf7413b8464c5b7d9f94242cfd0
Mariusz Nowak <medyk@medikoo.com>
2017-02-22 12:29:21 +0100

    Improve test

--- test/window-size.js
+++ test/window-size.js
@@ -3,6 +3,6 @@
 module.exports = function (t, a) {
 	a(typeof t.width, 'number', "Width");
 	a(typeof t.height, 'number', "Height");
-	a(t.width > 0, true);
-	a(t.height > 0, true);
+	a(t.width >= 0, true);
+	a(t.height >= 0, true);
 };
