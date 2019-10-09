9e96ca0ec2d5f1558be5871d978ab039c10975a0
Mariusz Nowak <mariusz@medikoo.com>
2019-10-09 16:13:17 +0200

    style: Prettify

--- test/_lib/supports-color-wrapper.js
+++ test/_lib/supports-color-wrapper.js
@@ -3,13 +3,9 @@ var supportsColor = require("../../lib/supports-color");
 
 // a wrapper function which automatically forces color mode to enabled during the tests.
 module.exports = function (fn) {
-    return function (t, a) {
-        supportsColor.enableColor();
-        try {
-            return fn(t, a);
-        }
-        finally {
-            supportsColor.autoDetectSupport();
-        }
-    };
+	return function (t, a) {
+		supportsColor.enableColor();
+		try { return fn(t, a); }
+		finally { supportsColor.autoDetectSupport(); }
+	};
 };
